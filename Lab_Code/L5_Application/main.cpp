c2bd01085620b753367ffa1fb2fde49cd52bcb6d
znic1967 <zackary.k.nicholson@gmail.com>
2018-03-09 20:56:20 -0800

    Code Template Finished

--- Lab_Code/L5_Application/main.cpp
+++ Lab_Code/L5_Application/main.cpp
@@ -5,17 +5,60 @@
 #include <string>
 using namespace std;
 
-void write_to_sram(string address, string data);
-char read_from_sram();
-void setDataIn();
+void write_to_sram(char* address, char* data);
+void read_from_sram(char* address);
 void pin_setter(char byte);
+int bit_checker(char* bits)
 
 //Push Test
 int main(void) {
-	printf("Welcome to the SJOne Board Interface")	
+	printf("Welcome to the SJOne Board Interface.\n");
+	setGPIOs();
+	char selector='0';
+	char address[8]='0';
+	char data[8]='0';
+	while(selector!='e')
+	{
+		printf("Select an option:\n");
+		printf("1) Write to SRAM.\n");
+		printf("2) Read from SRAM.\n");
+		printf("Enter \"e\" to quit.");
+		scanf("%c", selector);
+
+		if(selector=='1')
+		{
+			printf("Enter 8-bit Address to write to: ");
+			scanf("%s", address);
+			if(bit_checker(address)) 
+			{
+
+				printf("Enter 8-bit data to write: ");
+				scanf("%s",data);
+				if(bit_checker(data))
+				{
+					write_to_sram(address, data);
+				}
+				else printf("Data length not 8 bits.\n");
+			}
+			else printf("Address length not 8 bits.\n");
+		}
+		if (selector=='2')
+		{
+			printf("Enter 8-bit Address to read from: ");
+			scanf("%s", address);
+			if(bit_checker(address)) 
+			{
+				read_from_sram(address);
+			}
+			else printf("Address length not 8 bits.\n");
+		}
+	}
+	printf("SJOne Board Interface Exited.\n");
+	printf("Interface brought to you from countless 4AM SCE wiring wrapping sessions.\n");
+
 }
 
-void write_to_sram(char address, char data)
+void write_to_sram(char* address, char* data)
 {
 	pin_setter(address); //Set Address on SJOne
 
@@ -31,60 +74,44 @@ void write_to_sram(char address, char data)
 	addr_w.setHigh();
 	delay_ms(1); //Allow address to get to SRAM
 	addr_w.setLow();
-	
+
+	//Reset state machine
+	pin_setter(00000000);
+	cmd_w.setHigh();
+	delay_ms(1);
+	cmd_w.setLow();
+
+	pin_setter(01000100); //Latches cmd register
 	pin_setter(data); //Set Data Bits on SJOne
 	dataOut_w.setHigh();
-	pin_setter(00000000); //Clear cmd register
-	delay_ms(1);
-	pin_setter()
-
-
+	cmd_w.setHigh(); //Starts the state machine
+	printf("Write Operation Complete.\n");
 }
-string read_from_sram()
+void read_from_sram(char address)
 {
-	setdataIn();
-	//Set Address
-	addr_w.setHigh();
-	addr_w.setLow();
-	//Set Data
-	
-
-}
-void setDataIn
-{
-	bus_e#.setHigh();
-	
-	dataIn_e#.setLow();
-	addr_w.setLow();
-	setData(byte,&address); 
-	setData(byte,&address);
-	dataOut_w.setLow();
-	cmd_w.setLow();
-}
-void setDataOut()
-{
+	//Disable all 373s
 	dir_w.setHigh();
+	bus_en#.setHigh();
+	dataOut_w.setLow();
+	dataIn_e#.setHigh();
+	cmd_w.setLow();
+
+	//Pass Address to SRAM
 	bus_e#.setLow();
-	addr_w.setLow();
-	dataOut_w
-}
-void setAsOutput()
-{
-	dir_w.setAsOutput();
-	bus_e#.setAsOutput();
-	addr_w.setAsOutput();
-	dataOut_w.setAsOutput();
-	dataIn_e#.setAsOutput();
-	cmd_w.setAsOutput();
-}
-void setAsInput()
-{
-	dir_w.setAsInput();
-	bus_e#.setAsInput();
-	addr_w.setAsInput();
-	dataOut_w.setAsInput();
-	dataIn_e#.setAsInput();
-	cmd_w.setAsInput();
+	addr_w.setHigh();
+	delay_ms(1); //Allow address to get to SRAM
+	addr_w.setLow(); //Address latched.
+
+	//Reset state machine
+	pin_setter(00000000);
+	cmd_w.setHigh();
+	delay_ms(1);
+	cmd_w.setLow();
+
+	pin_setter(01001000); //Latches cmd register
+	dataIn_e#.setLow();
+	cmd_w.setHigh(); //Starts the state machine
+	printf("Read Operation Complete.\n");
 }
 
 void setGPIOs
@@ -102,7 +129,7 @@ void setGPIOs
 	GPIO  bus_e#(P0_1);
 	GPIO  addr_w(P2_1);
 	GPIO  dataOut_w(P2_2);
-	GPIO  datain_e#(P2_3);
+	GPIO  dataIn_e#(P2_3);
 	GPIO  cmd_w(P2_40);
 }
 
@@ -138,5 +165,13 @@ void pin_setter(char* byte)
 
 	if(bits[7]) a7.setHigh();
 	else a7.setLow();
+}
 
+int bit_checker(char* bits)
+{
+	int counter=0;
+	char str[100]=*bits;
+	for(int i=0; i<(unsigned)strlen(str); i++) counter++;
+	if (counter==8) return 1;
+	else return 0;
 }
